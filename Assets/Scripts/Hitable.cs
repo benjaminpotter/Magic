8dd69be25f97cea71adba523cd775cf7407f5347
unknown <reactiveneon@gmail.com>
2020-05-28 11:41:10 -0400

    added gitignore

---
+++ Assets/Scripts/Hitable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+// all things that can take damage from external sources have this component
+
+[RequireComponent(typeof(Living))] // if it can take damage it needs to be "living"
+public class Hitable : MonoBehaviour
+{
+    private Living living;
+
+    private void Start()
+    {
+        living = GetComponent<Living>();
+    }
+
+    public void Hit(float damage)
+    {
+        living.Hurt(damage);
+        Debug.Log(name + " took " + damage + " damage.");
+    }
+}
---
+++ Assets/Scripts/Living.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+// all things that have health in the world have this component
+public class Living : MonoBehaviour
+{
+    // this might use a scriptable object in the future
+    [SerializeField] private float maxHealth;
+    private float health;
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void Hurt(float damage)
+    {
+        health -= damage;
+
+        if (health < 0)
+            Die();
+    }
+
+    private void Die()
+    {
+        Debug.Log(name + " died.");
+    }
+}
